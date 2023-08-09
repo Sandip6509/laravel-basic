86bb5bb3730cd790a82670451ed4527e58570845
Sandip6509 <sandippatel3101@gmail.com>
2023-08-09 22:28:34 +0530

    Route Learn

---
+++ resources/views/index.blade.php
@@ -0,0 +1,1 @@
+<h1>You can create route with directly call view blade file from route directly</h1>
---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,1 @@
+<h2>you can create route with call controller method so, you can simply create controller method</h2>
