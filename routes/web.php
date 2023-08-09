6a287a5c929acaa489116d9d35ae9a9243cbf02d
Sandip6509 <sandippatel3101@gmail.com>
2023-08-09 22:28:07 +0530

    Route Learn

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('simple-route', function () {
+    return 'This is Simple Route';
+});
+Route::view('route-with-view','index');
+Route::get('route-controller',[TestController::class,'index']);
+Route::get('route-controller/{id}',[TestController::class,'show']);
+Route::resource('items',ItemController::class);
